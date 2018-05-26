1b047ca74fa3409c256fdd40097f111b7ecc7ce3
DanSoup <dansoup@hotmail.co.uk>
2018-05-26 12:14:59 +0100

    changed anoush to tristan

--- src/components/anoush.js
+++ src/components/anoush.js
@@ -5,7 +5,7 @@ function Home (props) {
 
     return <div id="Anoush">
         <div id="anoush-cage">
-            <img id="anoush-face" src="anoush.jpg"/>
+            <img id="anoush-face" src="daddy.png"/>
         </div>
         <iframe width="0" height="0" src="https://www.youtube.com/embed/OxMSd1-JSts?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </div>
