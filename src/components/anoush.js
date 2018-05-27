4895ff03c5b7b84b46efa440e678faace9c4ad9a
DanSoup <dansoup@hotmail.co.uk>
2018-05-27 23:32:52 +0100

    added more random options for the anoush easter egg page because now it's been shared by the team heir account

--- src/components/anoush.js
+++ src/components/anoush.js
@@ -3,9 +3,12 @@ import './anoush.css';
 
 function Home (props) {
 
+    const images = ['daddy.png', 'HappyCalz.png', 'lalzdo.png', 'blood.png', 'CptSoup.png']
+    const img = images[Math.floor(Math.random() * images.length)]
+
     return <div id="Anoush">
         <div id="anoush-cage">
-            <img id="anoush-face" src="daddy.png"/>
+            <img id="anoush-face" src={img}/>
         </div>
         <iframe width="0" height="0" src="https://www.youtube.com/embed/OxMSd1-JSts?rel=0&amp;controls=0&amp;showinfo=0&autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </div>
