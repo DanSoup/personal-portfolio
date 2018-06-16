2c642e42cbd320120158b47306c56da984e04da7
DanSoup <dansoup@hotmail.co.uk>
2018-06-17 00:20:43 +0100

    added animated intro screen

--- src/components/intro.js
+++ src/components/intro.js
@@ -2,9 +2,14 @@ import React, { Component } from 'react';
 
 function Intro (props) {
 
-    return <div>
-        Intro
+    return <div className="introPage">
+        <div className="introContainer">
+            <img className="introLogoTitle" src="img/logoTitle.svg"/>
+            <img className="introLogoLeft" src="img/logoLeft.svg"/>
+            <img className="introLogoRight" src="img/logoRight.svg"/>
+        </div>
     </div>
+
 }
 
 export default Intro;
